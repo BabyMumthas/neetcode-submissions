eae923e0f0fbf14e929ddbe78baf60b954a7c2fd
Baby mumthas  P <152773115+BabyMumthas@users.noreply.github.com>
2026-07-23 22:02:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            minPrice = min(minPrice, price);
+
+            int profit = price - minPrice;
+
+            maxProfit = max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+};
